cf55689680d00f1716758977909d26bfdfc1577f
Ian Marlowe <ianmarlowe@gmail.com>
2020-04-10 17:52:02 -0400

    Migrated to IntelliJ and added new form

--- VoiceChat_Server/src/SGUI.java
+++ VoiceChat_Server/src/SGUI.java
@@ -13,12 +13,12 @@ import javax.swing.JOptionPane;
  *
  * @author dosse
  */
-public class GUI extends javax.swing.JFrame {
+public class SGUI extends javax.swing.JFrame {
 
     /**
      * Creates new form GUI
      */
-    public GUI() {
+    public SGUI() {
         initComponents();
     }
 
@@ -149,13 +149,13 @@ public class GUI extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
@@ -166,7 +166,7 @@ public class GUI extends javax.swing.JFrame {
 
             @Override
             public void run() {
-                new GUI().setVisible(true);
+                new SGUI().setVisible(true);
             }
         });
     }
