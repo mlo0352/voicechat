bb0dce64fa60578cf9a1b9f6774fc77b87b398e2
Ian Marlowe <ianmarlowe@gmail.com>
2020-04-13 17:06:41 -0400

    Added set answers, increase round, tied to GUI. Set answers needs tied to client GUI.

---
+++ VoiceChat_Server/src/SetNewRoundHandler.java
@@ -0,0 +1,30 @@
+import com.sun.net.httpserver.*;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import org.json.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ian
+ */
+public class DeleteTeamHandler implements HttpHandler{
+    
+    private final Server mainServer;
+
+    public DeleteTeamHandler(Server mainServer) {
+        this.mainServer = mainServer;
+    }    
+    
+    public void handle(HttpExchange exchange) throws IOException {
+        String requestMethod = exchange.getRequestMethod();
+        exchange.sendResponseHeaders(501, 0);
+        exchange.close();
+    }
+            
+}
